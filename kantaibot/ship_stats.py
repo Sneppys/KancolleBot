fedb2bf790855032818fb2d5d52682247285b16c
Snep <snep@snep.pw>
2018-04-11 16:58:42 -0700

    automated ship data finding

--- kantaibot/ship_stats.py
+++ kantaibot/ship_stats.py
@@ -19,7 +19,21 @@ class ShipInstance:
     def new(sid):
         return ShipInstance(-1, sid)
 
-# Ship Type - The class of the ship
+# Suffixes at the end of a ship's name, using KC3 values
+
+SHIP_SUFFIXES = {0: "", 1: "Kai", 2: "Kai Ni", 3: "A", 4: "Carrier",
+                 5: "Carrier Kai", 6: "Carrier Kai Ni", 7: "zwei",
+                 8: "drei", 9: "Kai Ni A", 10: "Kai Ni B",
+                 11: "Kai Ni B", 12: "due", 13: "Kai Bo", 14: "dva",
+                 15: "Mk.II", 16: "Mk.II Mod.2", 17: "B Kai"}
+
+# Translations for Russian ships as KC3 does not store the english name
+
+SHIP_TRANSLATIONS = {u"Гангут": "Gangut", u"Верный": "Verniy",
+                     u"Октябрьская революция": "Oktyabrskaya Revolyutsiya",
+                     u"Ташкент": "Tashkent"}
+
+# Ship Type - The type of the ship, using KC3's internal type ids (Sometimes dupes)
 ALL_SHIP_TYPES = []
 class ShipType:
     def __init__(self, tid, discriminator, full_name):
@@ -28,23 +42,34 @@ class ShipType:
         self.full_name = full_name
         ALL_SHIP_TYPES.append(self)
 
-TYPE_DESTROYER = ShipType(0, "DD", "Destroyer")
-TYPE_LIGHT_CRUISER = ShipType(1, "CL", "Light Cruiser")
-TYPE_HEAVY_CRUISER = ShipType(2, "CA", "Heavy Cruiser")
-TYPE_LIGHT_CARRIER = ShipType(3, "CVL", "Light Carrier")
-TYPE_CARRIER = ShipType(4, "CV", "Carrier")
-TYPE_BATTLESHIP = ShipType(5, "BB", "Battleship")
+TYPE_DESTROYER = ShipType(1, "DD", "Destroyer")
+TYPE_LIGHT_CRUISER = ShipType(2, "CL", "Light Cruiser")
+TYPE_TORPEDO_CRUISER = ShipType(3, "CLT", "Torpedo Cruiser")
+TYPE_HEAVY_CRUISER = ShipType(4, "CA", "Heavy Cruiser")
+TYPE_AVIATION_CRUISER = ShipType(5, "CAV", "Aviation Cruiser")
+TYPE_BATTLESHIP = ShipType(6, "BB", "Battleship")
+TYPE_FAST_BATTLESHIP = ShipType(7, "FBB", "Fast Battleship")
+TYPE_AVIATION_BATTLESHIP = ShipType(8, "BBV", "Aviation Battleship")
+TYPE_LIGHT_CARRIER = ShipType(9, "CVL", "Light Carrier")
+TYPE_CARRIER = ShipType(10, "CV", "Carrier")
+TYPE_ARMORED_CARRIER = ShipType(11, "CVB", "Armored Carrier")
+TYPE_SEAPLANE_TENDER = ShipType(12, "AV", "Seaplane Tender")
+TYPE_SUBMARINE = ShipType(13, "SS", "Submarine")
+TYPE_SUBMARINE_ALT = ShipType(14, "SS", "Submarine")
+TYPE_AMPHIBIOUS_ASSAULT_SHIP = ShipType(15, "LHA", "Amphibious Assault Ship")
+TYPE_REPAIR_SHIP = ShipType(16, "AR", "Repair Ship")
+TYPE_SUBMARINE_TENDER = ShipType(17, "AS", "Submarine Tender")
+TYPE_DESTROYER_ALT = ShipType(19, "DD", "Destroyer")
+TYPE_TRAINING_CRUISER = ShipType(21, "CT", "Training Cruiser")
+TYPE_HEAVY_CRUISER_ALT = ShipType(23, "CA", "Heavy Cruiser")
+TYPE_SEAPLANE_TENDER_ALT = ShipType(24, "AV", "Seaplane Tender")
+TYPE_LIGHT_CRUISER_ALT = ShipType(28, "CL", "Light Cruiser")
+TYPE_FLEET_OILER = ShipType(29, "AO", "Fleet Oiler")
+TYPE_DESTROYER_ESCORT = ShipType(31, "DE", "Coastal Defense Ship")
+TYPE_LIGHT_CARRIER_ALT = ShipType(32, "CVL", "Light Carrier")
 
-
-# Ship Rarity - How rare the ship is to obtain randomly
-ALL_SHIP_RARITIES = []
-class ShipRarity:
-    def __init__(self, rid, rarity_value, name):
-        self.rid = rid
-        self.rarity_value = rarity_value
-        self.name = name
-        ALL_SHIP_RARITIES.append(self)
-
-RARITY_COMMON = ShipRarity(0, 100, "Common")
-RARITY_UNCOMMON = ShipRarity(1, 75, "Uncommon")
-RARITY_RARE = ShipRarity(2, 40, "Rare")
+def get_ship_type(tid):
+    r = [x for x in ALL_SHIP_TYPES if x.tid == int(tid)]
+    if len(r) > 0:
+        return r[0]
+    return None
--- tools/shipdbmanager.py
+++ tools/shipdbmanager.py
@@ -3,6 +3,8 @@ sys.path.append("../kantaibot/")
 import ship_stats
 import sqlite3
 import os
+import htmlparsing
+import json
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 DB_PATH = os.path.join(DIR_PATH, "../kantaidb.db")
@@ -13,23 +15,88 @@ def get_connection():
     return sqlite3.connect(DB_PATH)
 
 ship_id = input("Ship ID: ")
-ship_name = input("Ship Name: ")
-for type in ship_stats.ALL_SHIP_TYPES:
-    print(type.tid, type.discriminator)
-ship_type = input("Ship Type (id): ")
-for rarity in ship_stats.ALL_SHIP_RARITIES:
-    print(rarity.rid, rarity.name)
-ship_rarity = input("Ship Rarity (id): ")
-img_path1 = "%s %s" % (ship_id, ship_name)
 
 # kc3 path
-kc3_file_path = HOME_DIR + "AppData/Local/Google/Chrome/User Data/Default/Extensions/hkgmldnainaglpjngpajnnjfhpdjkohh/32.4.4_0/assets/img/ships"
-print(kc3_file_path)
-img_small_path = "%s.png" % (ship_id)
-img_small_damaged_path = "%s_d.png" % (ship_id)
+kc3_file_path = HOME_DIR + "/AppData/Local/Google/Chrome/User Data/Default/Extensions/hkgmldnainaglpjngpajnnjfhpdjkohh/32.4.4_0"
+print("Searching KC3 stored information...")
+db_path = "%s/data/WhoCallsTheFleet_ships.nedb" % (kc3_file_path)
+db_data = []
+with open(db_path, 'rb') as nedb:
+    for line in map(lambda x: x.decode('utf-8'), nedb):
+        data = json.loads(line)
+        db_data.append(data)
+kc3_id = 0
+kc3_entry = None
+
+def get_kc3_id(sid):
+    for entry in db_data:
+        if entry['no'] == int(sid):
+            return entry['id']
+
+def get_kc3_data(kcid):
+    for entry in db_data:
+        if entry['id'] == int(kcid):
+            return entry
+
+def get_ship_info(kcid, entry=None):
+    if (not entry):
+        entry = get_kc3_data(kcid)
+    sname = entry['name']['ja_jp'] if len(entry['name']['ja_romaji']) == 0 else entry['name']['ja_romaji']
+    sname = sname.title()
+    tname = sname
+    for o, t in ship_stats.SHIP_TRANSLATIONS.items():
+        tname = tname.replace(o, t)
+    stype = entry['type']
+    srarity = entry['rare']
+    suffix = 0
+    if 'suffix' in entry['name'] and entry['name']['suffix']:
+        suffix = ship_stats.SHIP_SUFFIXES[entry['name']['suffix']]
+        sname += " " + suffix
+        tname += " " + suffix
+    return sname, tname, stype, srarity
+
+
+kc3_id = get_kc3_id(ship_id)
+kc3_entry = get_kc3_data(kc3_id)
+ship_name, translated_name, ship_type, ship_rarity = get_ship_info(kc3_id, kc3_entry)
+if (kc3_id == 0):
+    print("ID not found")
+    input()
+    exit()
+type_discrim = ship_stats.get_ship_type(ship_type).discriminator
+print("Found ship '%s' of type %s and rarity %s with KC3 ID %s" % (ship_name, type_discrim, ship_rarity, kc3_id))
+img_small_path = "%s/assets/img/ships/%s.png" % (kc3_file_path, kc3_id)
+img_small_damaged_path = "%s/assets/img/ships/%s_d.png" % (kc3_file_path, kc3_id)
+
+# kcwiki data
+def perform_search(tname, stype, kcid):
+    print("Fetching images from Kancolle Wiki...")
+    typeobj = ship_stats.get_ship_type(stype)
+    imgs = htmlparsing.get_images_on_wiki_page(tname)
+    img_name_reg = "%s %s %03d Full" % (typeobj.discriminator, tname, int(kcid))
+    img_name_dmg = "%s Damaged" % (img_name_reg)
+    print("Searching for images: " + str([img_name_reg, img_name_dmg]))
+    try:
+        img_reg = [y for x, y in imgs.items() if x == img_name_reg].pop()
+        img_dmg = [y for x, y in imgs.items() if x == img_name_dmg].pop()
+        print("Found images: " + str([img_reg, img_dmg]))
+    except IndexError:
+        img_reg = None
+        img_dmg = None
+    return img_reg, img_dmg
+img_reg, img_dmg = perform_search(translated_name, ship_type, kc3_id)
+if (not img_reg or not img_dmg):
+    print("One or more images not found")
+    if ('prev' in kc3_entry['remodel']):
+        prev_id = kc3_entry['remodel']['prev']
+        print("Checking previous remodel...")
+        pname, ptname, ptype, prarity = get_ship_info(prev_id)
+        img_reg, img_dmg = perform_search(ptname, ptype, prev_id)
+        if (not img_reg or not img_dmg):
+            print("!! Could not find missing images")
 
 query = "REPLACE INTO ShipBase (ShipID, Name, Rarity, ShipType, Image_Default, Image_Damaged, Image_Small, Image_Small_Damaged) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');"
-query = query % (ship_id, ship_name, ship_rarity, ship_type, img_path1, img_path1, img_small_path, img_small_damaged_path)
+query = query % (ship_id, ship_name, ship_rarity, ship_type, img_reg, img_dmg, img_small_path, img_small_damaged_path)
 conn = get_connection()
 cur = conn.cursor()
 cur.execute(query)
