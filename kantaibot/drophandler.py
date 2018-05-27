3ec8a30c1f7378347e53c8f328d822779d28480b
Snep <snep@snep.pw>
2018-05-27 16:20:23 -0700

    forgot about drophandler.py

--- kantaibot/drophandler.py
+++ kantaibot/drophandler.py
@@ -1,20 +1,34 @@
+"""Handles random ship drops."""
 import ship_stats
 import random
-import sqlite3
 import os
-import userinfo
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-DB_PATH = os.path.join(DIR_PATH, "../usersdb.db") # hidden to git
 
-def get_connection():
-    return sqlite3.connect(DB_PATH)
 
 def get_basic_weight(ship):
+    """Get the base weight for a ShipBase."""
     return (9 - ship.rarity) * (10 - ship.rarity) ** 2
 
-def get_random_drop(owner, weight_function=get_basic_weight, only_droppable=False, only_craftable=False):
-    ships = ship_stats.get_all_ships(allow_remodel=False, only_droppable=only_droppable, only_craftable=only_craftable)
+
+def get_random_drop(owner, weight_function=get_basic_weight,
+                    only_droppable=False, only_craftable=False):
+    """Get a random ship drop, as a ShipInstance.
+
+    Parameters
+    ----------
+    owner : int
+        Discord ID of the owner of the drop.
+    weight_function : function
+        Function used to determine weight based on passed ShipBase.
+    only_droppable : bool
+        If True, only select from the ships which can be dropped.
+    only_craftable : bool
+        If True, only select from the ships which can be crafted.
+    """
+    ships = ship_stats.get_all_ships(allow_remodel=False,
+                                     only_droppable=only_droppable,
+                                     only_craftable=only_craftable)
 
     total_pool = sum(weight_function(s) for s in ships)
     val = random.randrange(total_pool)
@@ -25,12 +39,34 @@ def get_random_drop(owner, weight_function=get_basic_weight, only_droppable=Fals
             break
     return ship_stats.ShipInstance.new(11, owner)
 
-def get_drop_chances(weight_function=get_basic_weight):
-    ships = ship_stats.get_all_ships(allow_remodel=False)
+
+def get_drop_chances(weight_function=get_basic_weight, only_droppable=False,
+                     only_craftable=False):
+    """Get the chances that each rarity can drop.
+
+    Parameters
+    ----------
+    weight_function : function
+        Function used to determine weight based on passed ShipBase.
+    only_droppable : bool
+        If True, only select from the ships which can be dropped.
+    only_craftable : bool
+        If True, only select from the ships which can be crafted.
+
+    Returns
+    -------
+    list
+        List of size 8, floats equalling percent the respective rarity can
+        drop.
+    """
+    ships = ship_stats.get_all_ships(allow_remodel=False,
+                                     only_droppable=only_droppable,
+                                     only_craftable=only_craftable)
 
     total_pool = sum(weight_function(s) for s in ships)
     totals = [0] * 8
     for rarity in range(8):
-        rtotal = sum(weight_function(s) for s in ships if s.rarity == rarity + 1)
+        rtotal = sum(weight_function(s) for s in ships
+                     if s.rarity == rarity + 1)
         totals[rarity] = rtotal
     return list(map(lambda x: x / total_pool, totals))
